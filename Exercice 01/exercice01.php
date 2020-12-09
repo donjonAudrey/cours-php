3636ef33d77bd44cf5494b0aa035d9827cdb507e
Audrey <danataelia@gmail.com>
2020-12-09 11:46:52 +0100

    fin de l'exercice 01

---
+++ Exercice 01/exercice01.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Exercice 01 : PHP</title>
+    </head>
+    <body>
+    <h1>Mon premier petit exercice en php</h1>
+    <?php
+    echo '<ul>
+        <li>Pomme</li>
+        <li>Fraise</li>
+        <li>Cerise</li>
+        <li>Abricot</li>
+        <li>Banane</li>
+    </ul>'
+    ?>
+    </body>
+</html>
