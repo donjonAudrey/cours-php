55ea17cc49ce3714283f041e5c8c5d8248e5a0c5
Audrey <danataelia@gmail.com>
2020-12-09 16:23:05 +0100

    maj correctifs de l'exercice 03

--- Exercice 03/exercice03.php
+++ Exercice 03/exercice03.php
@@ -20,12 +20,22 @@ $admin  = true;
         </style>
     </head>
     <body>
+        <h1>Exercice 03</h1>
         <?php
-            if($admin === false){
-                echo '<p>Bienvenu Mr l\'admin Je te prierais de bien vouloir aller sur ce lien : </p><a href="#">Le lien ici</a>';
-            }else{
-                echo '<p class="notAdmin">Vous ne faites pas partie des admins de ce site, accès non autorisé !</p>';
-            }
+        //Même si la balise PHP se ferme avant la fermeture du if, le code HTML déclaré entre sera considéré comme faisant partie de la condition.
+        if($admin){
+
+            ?>
+            <p>Bienvenu Mr l'admin Je te prierais de bien vouloir aller sur ce lien : </p><a href="https://resize-elle.ladmedia.fr/rcrop/1024,1024/img/var/plain_site/storage/images/societe/news/phenomene-que-cache-notre-addiction-aux-images-de-chatons-3869316/93623270-2-fre-FR/Phenomene-que-cache-notre-addiction-aux-images-de-chatons.jpg">Le lien ici</a>
+            <?php
+
+        }else{
+
+            ?>
+            <p class="notAdmin">Vous ne faites pas partie des admins de ce site, accès non autorisé !</p>
+            <?php
+        }
+
         ?>
     </body>
 </html>
